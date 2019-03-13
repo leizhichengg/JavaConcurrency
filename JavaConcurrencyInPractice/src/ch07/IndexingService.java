5e772e1106971c95c02e51ac7e1c76b7cc80e05b
withlzc <withlzc@gmail.com>
2019-03-13 10:40:39 +0800

    update ch07

---
+++ JavaConcurrencyInPractice/src/ch07/IndexingService.java
@@ -0,0 +1,78 @@
+package ch07;
+
+import java.io.File;
+import java.io.FileFilter;
+import java.util.concurrent.BlockingQueue;
+
+/**
+ * @Author: lei
+ * @Description:
+ * @Date: Created in 2019-03-13 08:56
+ */
+public class IndexingService {
+    private static final File POISON = new File("");
+    private final IndexerThread consumer = new IndexerThread();
+    private final CrawlerThread producer = new CrawlerThread();
+    private final BlockingQueue<File> queue;
+    private final FileFilter fileFilter;
+    private final File root;
+
+
+    class CrawlerThread extends Thread {
+        @Override
+        public void run() {
+            try {
+                crawl(root);
+            } catch (InterruptedException e) {
+
+            } finally {
+                while (true) {
+                    try {
+                        queue.put(POISON);
+                        break;
+                    } catch (InterruptedException e) {
+
+                    }
+                }
+            }
+        }
+
+
+        public void crawl(File root) throws InterruptedException {
+
+        }
+    }
+
+    class IndexerThread extends Thread {
+        @Override
+        public void run() {
+            try {
+                while (true) {
+                    File file = queue.take();
+                    if (file == POISON) {
+                        break;
+                    } else {
+                        indexFile(file);
+                    }
+                }
+            } catch (InterruptedException e) {
+
+            }
+        }
+    }
+
+    public void start() {
+        producer.start();
+        consumer.start();
+    }
+
+    public void stop() {
+        producer.interrupt();
+    }
+
+    public void awaitTermination() throws InterruptedException {
+        consumer.join();
+    }
+
+}
+
